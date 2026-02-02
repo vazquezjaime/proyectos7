e593d4b8eb05895875e90bd1d78c9f86b84542cf
Jaime <vazquez.jaime@gmail.com>
2026-02-02 15:16:45 -0600

    botones

--- app.py
+++ app.py
@@ -35,3 +35,11 @@ fig = px.scatter(
 )
 
 fig.show()
+
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    ...
